660e6dd9f8e455a8c62019c3c901f72bdd421063
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-03-26 23:38:44 -0400

    fonction textarea debuter

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let notes = document.getElementById("noteContainer");
 
 let btn = document.getElementById("noteBtn");
 
+function noteContent() {}
+
 btn.addEventListener("click", () => {
 	console.log("success");
 });
