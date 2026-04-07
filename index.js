2d80595a5cc3bc53741bcd673ab8e228bf076ddc
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-04-06 22:53:21 -0400

    starting over

--- index.js
+++ index.js
@@ -1,44 +1,9 @@
 //Declaration et initialisatio de nos variables
 let notes = document.getElementById("noteContainer");
-
 let btn = document.getElementById("noteBtn");
 
 function noteContent() {
-	//creation du textarea element.
-	let newNote = document.createElement("textarea");
-	newNote.placeholder = "Empty Note";
-
-	//Ajoutons la note cree sur le container de la note.
-	notes.prepend(newNote);
-
-	//Recuperons la note sauvegardee
-	let savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
-
-	//ajoutons les notes dans un tableau
-	savedNotes.push("");
-	localStorage.setItem("notes", JSON.stringify(savedNotes));
-
-	//Sauvegarder quand l'utilisateur tape
-	newNote.addEventListener("input", () => {
-		let allNotes = document.querySelectorAll("textarea");
-		let notesArray = [];
-
-		allNotes.forEach(function (note) {
-			notesArray.push(note.value);
-		});
-
-		localStorage.setItem("notes", JSON.stringify(notesArray));
-	});
-
-	//notes sauvegarder
-	savedNotes.forEach(function (note) {
-		let textarea = document.createElement("textarea");
-		textarea.placeholder = "Empty Note";
-		textarea.value = note;
-
-		notes.prepend(textarea);
-	});
-
+	
 	//Ajoutons une confirmation de suppression
 	newNote.addEventListener("dblclick", () => {
 		var userConfirmed = confirm("Do you want to delete this note ?");
