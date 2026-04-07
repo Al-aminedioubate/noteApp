bfe7460545b5f1a88895e37f82cdb97b21fdcd1f
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-04-06 23:03:52 -0400

    recuperation de note terminer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let notes = document.getElementById("noteContainer");
 let btn = document.getElementById("noteBtn");
 
+//la fonction permettant de creer la note
 function createNote(id, content) {
 	const textarea = document.createElement("textarea");
 	textarea.classList.add("container-notes");
@@ -9,6 +10,11 @@ function createNote(id, content) {
 	textarea.value = content;
 
 	return textarea;
+}
+
+//fonction permettant de recuperer la note sauvegardee dans local storage.
+function getNotes() {
+	return JSON.parse(localStorage.getItem("note") || "[]");
 }
 
 btn.addEventListener("click", () => {
